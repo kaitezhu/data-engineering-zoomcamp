d362305f4ec5113581987d7048bd1e731add8d71
Kaite Zhu <1024812321@qq.com>
2024-04-20 15:00:23 +1000

    Added main.tf and variables.tf

--- cohorts/2024/01-docker-terraform/1_terraform_gcp/terraform_demo/main.tf
+++ cohorts/2024/01-docker-terraform/1_terraform_gcp/terraform_demo/main.tf
@@ -8,13 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project = "xenon-depth-420805"
-  region  = "australia-southeast2-a"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "xenon-depth-420805-terra-bucket"
-  location      = "AUSTRALIA-SOUTHEAST2"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
 
@@ -26,4 +27,9 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
